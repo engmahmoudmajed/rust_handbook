9bc42be5bcf6642d4baa66ad48be7020b2174fde
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2026-07-11 22:31:08 +0300

    finish studt References and Borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-fn main() {
-  let mut name = String::from("Boris");
-	println!("{name}");
-	name.push_str(" Pask");
-	println!("{name}");
+// 
+/* 
+what mean ownership -> mean who responseible to clean data from memorey  
+
+*/
+fn main(){
+  // bool use copy trait 
+  let registration = [true,false,true]; 
+  let registration_tuples = (true,false,true); 
+  let first = registration[0];
+  println!("{first} and {registration:?}");
+  // use heap data
+  let languages = [String::from("Rust"),String::from("javascript"),String::from("c"),String::from("Python")];
+  // let first = languages[0] ; //will make error because ownership of string 
+  // sloutions
+  let first_arr = languages[0].clone();
+  let second = &languages[0];
+  println!("{first_arr} and {second}");
+  //use tuples
+  
+  // same as array
+  let first_tup = registration_tuples.0.clone();
+  let second_tup = &registration_tuples.1;
+  println!("{first_tup} and {second_tup}");
 }
-
-
-
-
 
