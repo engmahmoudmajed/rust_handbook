3d62ffa7d58dcb71078292eb0174e3833845c035
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2026-07-22 02:35:51 +0300

    finish study rust for today struct p-1

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-// 
-/* 
-what mean ownership -> mean who responseible to clean data from memorey  
+// Passing Structs into a Function
 
-*/
-fn main(){
-  // bool use copy trait 
-  let registration = [true,false,true]; 
-  let registration_tuples = (true,false,true); 
-  let first = registration[0];
-  println!("{first} and {registration:?}");
-  // use heap data
-  let languages = [String::from("Rust"),String::from("javascript"),String::from("c"),String::from("Python")];
-  // let first = languages[0] ; //will make error because ownership of string 
-  // sloutions
-  let first_arr = languages[0].clone();
-  let second = &languages[0];
-  println!("{first_arr} and {second}");
-  //use tuples
-  
-  // same as array
-  let first_tup = registration_tuples.0.clone();
-  let second_tup = &registration_tuples.1;
-  println!("{first_tup} and {second_tup}");
+struct Coffee {
+  price: f64,
+  name: String,
+  is_hot: bool,
 }
 
+
+
+fn main() {
+  let mocha = make_coffee(4.66, String::from("Latte"), true);
+
+
+
+
+}
+
+fn make_coffee( price: f64,name: String,is_hot: bool) -> Coffee {
+
+  Coffee {
+    name,
+    price,
+    is_hot,
+  }
+}
