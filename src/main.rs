4b3e2312c4d38232a897689de55460051c01f409
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2026-07-28 08:48:11 +0300

    stopped for today

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
-// Passing Structs into a Function
 
-struct Coffee {
-  price: f64,
-  name: String,
-  is_hot: bool,
-}
+// Define Methods 
+// Method : function that belongs to an instance 
+// like .push .pull 
+// method like function -> accept params return values
+// but like we add behavior owend by struct
 
 
 
 fn main() {
-  let mocha = make_coffee(4.66, String::from("Latte"), true);
-
-
-
-
-}
-
-fn make_coffee( price: f64,name: String,is_hot: bool) -> Coffee {
-
-  Coffee {
-    name,
-    price,
-    is_hot,
-  }
+   
 }
