43e051d2d5e61155559f46695c9e40843898bd19
Eng Mahmoud Majed <mahmoudmajed404@gmail.com>
2026-08-15 23:55:49 +0300

    finish struct

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-
-// Define Methods 
-// Method : function that belongs to an instance 
-// like .push .pull 
-// method like function -> accept params return values
-// but like we add behavior owend by struct
+fn main(){
 
 
-
-fn main() {
-   
+    
 }
